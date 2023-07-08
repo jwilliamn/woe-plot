1084fb5e9d6aa7d33c7570364f854eafc5bb2599
Jaspers W. Huanay <hwilliamnq@gmail.com>
2023-07-08 19:05:42 +0300

    woebin_plot2 done

--- __init__.py
+++ __init__.py
@@ -1,7 +1,4 @@
-"""The ``ibk.drift`` package contains tools to
-    analyze the changes in data dristibution
-    during modelling or to monitor when the data is
-    in production
+"""
 
 .. note::
     This package is experimental and the tools may be subject
